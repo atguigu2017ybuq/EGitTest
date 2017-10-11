433377bf8f110950bd147530895fa1079c6c0022
shuai_global <shuai_global@atguigu.com>
2017-10-11 16:16:52 +0800

    for conflict

--- src/main/java/com/atguigu/git/test/Student.java
+++ src/main/java/com/atguigu/git/test/Student.java
@@ -4,6 +4,8 @@ public class Student {
 	
 	public static void main(String[] args) {
 		System.out.println("aaaaa.....");
+		System.out.println("aaaaa.....");
+		System.out.println("aaaaa.....");
 	}
 
 }
