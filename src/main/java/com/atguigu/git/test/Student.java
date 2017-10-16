d1496406b84a237a60c38c6f0f2e997b4f9c8b14
qiyonghui <qyh@163.com>
2017-10-16 16:07:09 +0800

    新的修改

--- src/main/java/com/atguigu/git/test/Student.java
+++ src/main/java/com/atguigu/git/test/Student.java
@@ -3,12 +3,13 @@ package com.atguigu.git.test;
 public class Student {
 	
 	public String getDataMessage() {
-		return "student message for test jenkins[Deploy again]";
+		return "a excellent student message for test jenkins[create new edition]";
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("a1aa......222222222");
 		System.out.println("aaaaa.....[BBBBBBBBBBBB]");
+		System.out.println("aaaaa.....[CCCCCCC]");
 		System.out.println("aaaaa.....[AAAAAAAAAAAA]");
 		System.out.println("aaaaa.....");
 	}
