5f2c30014250232a03252fdd3980714cf24315ef
fengbin_global <fengibin@qq.com>
2017-11-22 19:39:56 +0800

    how do you do

--- src/main/java/com/atguigu/git/test/Student.java
+++ src/main/java/com/atguigu/git/test/Student.java
@@ -6,11 +6,6 @@ public class Student {
 		return "a excellent student message for test jenkins[create new edition]";
 	}
 	
-	public static void main(String[] args) {
-		System.out.println("a1aa......222222222");
-		System.out.println("aaaaa.....[BBBBBBBBBBBB]");
-		System.out.println("aaaaa.....[CCCCCCC]");
-		System.out.println("aaaaa.....[AAAAAAAAAAAA]");
 		System.out.println("aaaaa.....");
 	}
 
